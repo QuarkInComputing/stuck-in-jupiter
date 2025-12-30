27e73b5b1bc9390975f9e4a2136a3597200fb715
QuarkInComputing <jackmcleanwilson@gmail.com>
2025-12-30 10:56:15 +0000

    Edited config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://stuck-in-jupiter.github.io',
+    site: 'https://quarkincomputing.github.io',
 });
